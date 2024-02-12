7d3eafae31260846569bee754deffa2cb36bcc4c
MaxHiit <pro.maxcode@gmail.com>
2024-02-12 14:54:00 +0100

    chore(package): setup next js domain | add new app package

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'uploadthing.com'
+			},
+			{
+				protocol: 'https',
+				hostname: 'utfs.io'
+			}
+		],
+		unoptimized: true
+	}
+};
 
 export default nextConfig;
