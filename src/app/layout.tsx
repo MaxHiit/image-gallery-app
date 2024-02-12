c213de3ed7c308070ff07bd3f447d70c6ba89056
MaxHiit <pro.maxcode@gmail.com>
2024-02-12 14:44:31 +0100

    feat(page): global app layout | home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Header } from '@/components/header';
+import Footer from '@/components/footer';
 import { Toaster } from '@/components/ui/sonner';
-import { UploadModalProvider } from '@/provider/upload-modal';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
@@ -21,10 +21,10 @@ export default function RootLayout({
 	return (
 		<html lang='en'>
 			<body className={inter.className}>
-				<Header />
 				<Toaster position='top-center' />
-				<UploadModalProvider />
+				<Header />
 				{children}
+				<Footer />
 			</body>
 		</html>
 	);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-	return <main className='min-h-full'></main>;
+import { Suspense } from 'react';
+import { getImages } from '@/actions';
+import { GalleryHeader, GalleryGrid } from '@/components/Gallery';
+import { Loader } from '@/components/loader';
+
+export default async function Home() {
+	const images = await getImages();
+
+	return (
+		<main className='min-h-full px-8 py-5 md:max-w-[1500px] mx-auto space-y-4'>
+			<GalleryHeader images={images} />
+			<Suspense fallback={<Loader visible={true} />}>
+				<GalleryGrid images={images} />
+			</Suspense>
+		</main>
+	);
 }
