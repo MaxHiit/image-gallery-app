1c79956d5bd90623eecdd3ec36aa6c8d6992b6b7
MaxHiit <pro.maxcode@gmail.com>
2024-02-12 15:05:53 +0100

    feat(components): gallery layout

---
+++ src/components/Gallery/gallery-grid.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import type { UploadsRecord } from '@/xata';
+import { saveAs } from 'file-saver';
+import { Download } from 'lucide-react';
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import { Zoom } from '@/components/zoom-image';
+
+interface GalleryGridProps {
+	images: UploadsRecord[];
+}
+
+export const GalleryGrid = ({ images }: GalleryGridProps) => {
+	if (images.length === 0) {
+		return (
+			<div className='min-h-[58.8vh] md:h-[68.3dvh] flex items-center justify-center'>
+				<p>No images</p>
+			</div>
+		);
+	}
+
+	return (
+		<div className='grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-6'>
+			{images.map((image) => (
+				<div key={image.key}>
+					<Zoom>
+						<div className='relative h-[400px] md:h-[300px]'>
+							<Image
+								className='w-full h-full object-cover object-top rounded-lg '
+								fill
+								sizes='100vw'
+								src={image.url ?? ''}
+								alt={image.name ?? ''}
+								placeholder='blur'
+								blurDataURL={image.url ?? ''}
+								loading='lazy'
+							/>
+						</div>
+					</Zoom>
+					<Button
+						variant='outline'
+						title='Download this image'
+						className='mt-4 flex items-center w-full'
+						onClick={() => saveAs(image.url ?? '', image.name ?? '')}
+					>
+						<Download className='size-4 mr-2' aria-hidden='true' />
+						<span>Dowload</span>
+					</Button>
+				</div>
+			))}
+		</div>
+	);
+};
---
+++ src/components/Gallery/gallery-header.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { saveAs } from 'file-saver';
+import JSZip from 'jszip';
+import type { UploadsRecord } from '@/xata';
+import { UploadDialog } from '@/components/UploadFile';
+import { Button } from '@/components/ui/button';
+
+interface GalleryHeaderProps {
+	images: UploadsRecord[];
+}
+
+export const GalleryHeader = ({ images }: GalleryHeaderProps) => {
+	const handleDownload = async () => {
+		const zip = new JSZip();
+
+		await Promise.all(
+			images.map(async (image, index) => {
+				if (image.url) {
+					try {
+						const response = await fetch(image.url);
+						const blob = await response.blob();
+						zip.file(`${image.name || `image-${index}`}`, blob);
+					} catch (error) {
+						console.error('Error downloading image:', error);
+					}
+				}
+			})
+		);
+
+		try {
+			const content = await zip.generateAsync({ type: 'blob' });
+			saveAs(content, 'images.zip');
+		} catch (error) {
+			console.error('Error generating zip file:', error);
+		}
+	};
+
+	return (
+		<div className='sticky top-0 z-10 bg-background py-3 flex flex-col items-center justify-between md:flex-row space-y-3 md:space-y-0 md:space-x-4'>
+			<UploadDialog />
+			<Button
+				variant='outline'
+				className='w-full md:w-fit'
+				onClick={handleDownload}
+				disabled={images.length < 1}
+			>
+				Download all
+			</Button>
+		</div>
+	);
+};
